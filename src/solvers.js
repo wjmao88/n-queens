96a7bc0c19a91bf454bd8369a95675db016f68fe
wjmao88 <wjmao88@gmail.com>
2014-03-07 00:11:44 -0800

    loop solution now works

--- src/solvers.js
+++ src/solvers.js
@@ -174,55 +174,38 @@ window.countNQueensSolutions = function(n) {
   return counter;
 };
 
-window.countNQueensSolutionsBit = function(n) {
-  console.log('============= START ' + n + ' =============');
+window.countNQueensSolutions = function(n) {
+  console.log('============= BIT START ' + n + ' =============');
   if (n === 1 || n === 0){
     return 1;
   }
   var counter = 0;
-  var rowPrune = 0;
   var colPrune = 0;
   var majorPrune = 0;
   var minorPrune = 0;
 
-  var findSolution = function (numQueens) {
-    // Base Case
-    if (numQueens === 0) {
-      counter++;
-      return;
-    }
+  var max = n;
+  var n = 1 << n;
+  var iterCount = 0;
+  var findSolution = function (row) {
+    for (var col = 1; col < n; col = col << 1){
+      iterCount++;
+      if (!(col & colPrune)){
+        if( !( ((col * row) & minorPrune) | (((col << max)/row) & majorPrune)) ) {
+          if (row === 1){
+            counter++;
+          } else {
+            colPrune += col;
+            majorPrune += (col << max)/row;
+            minorPrune +=  row*col;
 
+            findSolution(row >> 1);
 
-    // ((colPrune | (1 << colIndex)) ^ colPrune) => pruned : 0, unpruned : !0
-    // we want the expression to be false if any 0 is contained
-    // and the expression will be false if the reverse of any one is a 0
-    // which is !(!a | !b | !c)
-
-    // Increment across board
-    for (var rowIndex = 0; rowIndex < n; rowIndex++){
-      //mylog('outer');
-      if ( !((rowPrune | (1 << rowIndex)) ^ rowPrune) ){
-        break;
-      }
-      for (var colIndex = 0; colIndex < n; colIndex++){
-        //mylog('inner');
-        if(!( !((colPrune | (1 << colIndex)) ^ colPrune) |
-              !((majorPrune | (1 << (colIndex - rowIndex + n))) ^ majorPrune) |
-              !((minorPrune | (1 << (colIndex + rowIndex + n))) ^ minorPrune ) ) ) {
-
-          //prune
-          rowPrune = rowPrune + (1 << rowIndex);
-          colPrune = colPrune + (1 << colIndex);
-          majorPrune = majorPrune + (1 << (colIndex - rowIndex + n));
-          minorPrune = minorPrune + (1 << (colIndex + rowIndex + n));
-
-          findSolution(numQueens - 1);
-
-          //unprune
-          rowPrune = rowPrune - (1 << rowIndex);
-          colPrune = colPrune - (1 << colIndex);
-          majorPrune = majorPrune - (1 << (colIndex - rowIndex + n));
-          minorPrune = minorPrune - (1 << (colIndex + rowIndex + n));
+            //unprune
+            colPrune -= col;
+            majorPrune -= (col << max)/row;
+            minorPrune -= col*row;
+          }
         }
       }
     }
@@ -230,103 +213,69 @@ window.countNQueensSolutionsBit = function(n) {
 
   // Start solution search
   var begin = new Date().getTime();
-  findSolution(n);
+  findSolution(n >> 1);
   var duration = new Date().getTime() - begin;
-  mylog(n, counter, duration);
+  mylog('solutions ', max, counter);
+  mylog('count ', iterCount + ' in ms: ' + duration);
   return counter;
 };
 
-window.countNQueensSolutions2 = function(n) {
-  console.log('============= START ' + n + ' =============');
+window.countNQueensSolutions = function(n) {
+  console.log('============= BIT LOOP START ' + n + ' =============');
   if (n === 1 || n === 0){
     return 1;
   }
   var counter = 0;
-  var rowPrune = 0;
   var colPrune = 0;
-  var majorPrune = 0;
-  var minorPrune = 0;
+  var major = 0;
+  var minor = 0;
 
-  // Start solution search=================================
-  var begin = new Date().getTime();
+  var max = n;
+  var n = 1 << (n-1);
 
-  var rowIndex = 0;
-  var colIndex = 0;
-  var queens = n;
-
+  var row = n;
+  var col = 1;
   var callStack = [];
-  var prevCall;
-
-  var rowIndex, colIndex;
-
-  // ((colPrune | (1 << colIndex)) ^ colPrune) => pruned : 0, unpruned : !0
-  // we want (!0 && !0 && !0), which is !(0 | 0 | 0)
-  //
-  while(rowIndex < n || colIndex < n || queens < n){
-    logger('nwrow');
-    boardIteration :
-    while(rowIndex < n){
-      if ( ((rowPrune | (1 << rowIndex)) ^ rowPrune) ){
-        rowIndex++;
-        break;
-      }
-      while (colIndex < n){
-        if(!( !((colPrune | (1 << colIndex)) ^ colPrune) |
-              !((majorPrune | (1 << (colIndex - rowIndex + n))) ^ majorPrune) |
-              !((minorPrune | (1 << (colIndex + rowIndex + n))) ^ minorPrune ) ) ) {
-        //we are at a valid position
-        //prune
-          logger('valid');
-          if (queens === 1) {
-            logger('solut');
+  var iterCount = 0;
+  // Start solution search
+  //=======================================================
+  var begin = new Date().getTime();
+  out:
+  while(true){
+    newRow:
+    while (col <= n){
+      iterCount++;
+      if (!(col & colPrune)){
+        if( !( ((col * row) & minor) | (((col << max)/row) & major)) ) {
+          if (row === 1) {
             counter++;
-            colIndex++;
           } else {
-            logger('prune');
-            rowPrune = rowPrune + (1 << rowIndex);
-            colPrune = colPrune + (1 << colIndex);
-            majorPrune = majorPrune + (1 << (colIndex - rowIndex + n));
-            minorPrune = minorPrune + (1 << (colIndex + rowIndex + n));
-            queens--;
-            callStack.push([rowIndex, colIndex]);
-            rowIndex = 0;
-            colIndex = 0;
-            logger('newst');
+            colPrune += col;
+            major += (col << max)/row;
+            minor += col*row;
+            callStack.push(col);
+            row = row >> 1;
+            col = 1;
+            break newRow;
           }
         }
-      } //else is skipped
-      //colindex++ here
-    }//end boardIteration
-    //unprune
-    logger('itrtn');
-    if (++rowIndex < n){
-      colIndex = 0;
-    } else if (queens < n){
-      logger('unprn');
-      prevCall = callStack.pop();
-      rowIndex = prevCall[0];
-      colIndex = prevCall[1];
-      rowPrune = rowPrune - (1 << rowIndex);
-      colPrune = colPrune - (1 << colIndex);
-      majorPrune = majorPrune - (1 << (colIndex - rowIndex + n));
-      minorPrune = minorPrune - (1 << (colIndex + rowIndex + n));
-      queens++;
-      colIndex++;
+      }
+      col = col << 1;
     }
-    logger('ended');
-  }//end while
-
-  function logger(msg){
-    var str = '';
-    for (var i=0; i<queens; i++){
-      str += '=';
+    if (row === n){
+      break out;
+    } else if (col !== 1){
+      col = callStack.pop();
+      row = row << 1;
+      colPrune -= col;
+      major -= (col << max)/row;
+      minor -= col*row;
+      col = col << 1;
     }
-    str += msg + ' r: ' + rowIndex + ' c: ' + colIndex + ' s: ' + counter;
-    str += ' rp: ' + rowPrune + ' cp: ' + rowPrune + ' mp: ' + majorPrune + ' np: ' + minorPrune
-    console.log(str);
-  }
+  }//end rows
   //=======================================================
   var duration = new Date().getTime() - begin;
-  mylog(n, counter, duration);
+  mylog('solutions ', max, counter);
+  mylog('count ', iterCount + ' in ms: ' + duration);
   return counter;
 };
